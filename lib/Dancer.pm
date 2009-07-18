71f7e3c87ff6f532c24855088e204473d1697c1e
Alexis Sukrieh <sukria@sukria.net>
2009-07-18 11:55:26 +0200

    more stuff in the POD

--- lib/Dancer.pm
+++ lib/Dancer.pm
@@ -2,12 +2,14 @@ package Dancer;
 
 use strict;
 use warnings;
-use vars qw($VERSION @EXPORT);
+use vars qw($VERSION $AUTHORITY @EXPORT);
 
 use Dancer::Registry;
 use HTTP::Server::Simple::CGI;
 use base 'Exporter', 'HTTP::Server::Simple::CGI';
 
+$AUTHORITY = 'SUKRIA';
+$VERSION = '0.1';
 @EXPORT = qw(
     get 
     post 
@@ -70,20 +72,60 @@ This is under heavy development
 
 =head1 DESCRIPTION
 
-Dancer is framework for writing web application with minimal effort. It was 
+Dancer is a framework for writing web applications with minimal effort. It was
 inspired by Sinatra, from the Ruby's community.
 
-Dancer is here to provide the simpliest way for writing web application.
+Dancer is here to provide the simpliest way for writing a web application.
 
 =head1 USAGE
 
 As soon as Dancer is imported to a script, that script becomes a webapp.
 
-=head1 TODO
+All the script has to do is to declare a list of B<routes> which are basically:
+    
+=over 4
+
+=item a B<pattern> for matching HTTP requests 
+
+=item a B<block of code> that has to return a string supposed to be the content
+to render.
+
+=back
+
+Routes are defined for a given HTTP method (get or post). For each method
+supported, a keyword is exported by the module. 
+
+Here is an example of a route definition:
+
+    get '/hello/:name' => sub {
+        my ($params) = @_;
+
+        # do something important here
+        
+        return "Hello ".$params->{name};
+    };
+
+The route is defined for the method 'get', so only GET requests will be honoured
+by that route.
+
+The route pattern contains a token (a word prefixed with ':'). Each token found
+in a route patter is used as a named-pattern match. Any match will be set in
+the params hashref given to the B<route action>.
+
+The route action is the code reference declared, it receives the params as its
+first argument. This hashref is a merge of the route pattern matches and de
+request params.
+
+=head1 EXAMPLE
+
+This is a possible webapp created with Dancer :
 
     #!/usr/bin/perl
+    
+    # make this script a webapp
     use Dancer;
 
+    # declare routes/actions
     get '/' => sub { 
         "Hello World";
     };
@@ -92,5 +134,21 @@ As soon as Dancer is imported to a script, that script becomes a webapp.
         "Hello ".$params{name}"
     };
 
+    # run the webserver
+    Dancer->dance;
+
+=head1 AUTHOR
+
+This module has been written by Alexis Sukrieh <sukria@cpan.org>
+
+=head1 LICENSE
+
+This module is free software and is published under the same
+terms as Perl itself.
+
+=head1 SEE ALSO
+
+The concept behind this module comes from the Sinatra ruby project, 
+see L<http://www.sinatrarb.com> for details.
 
 =cut
