0e06303c135558ab07fc28b6012f55bd4b46c9a9
Alexis Sukrieh <sukria@sukria.net>
2009-08-01 15:41:00 +0200

    some documentation about command-line options

--- lib/Dancer.pm
+++ lib/Dancer.pm
@@ -143,11 +143,9 @@ project, Dancer is what you need.
 
 =head1 USAGE
 
-As soon as Dancer is imported to a script, that script becomes a webapp.
-
-All the script has to do is to declare a list of B<routes>.
-    
-A route handler is composed by an HTTP method, a path pattern and a code block.
+As soon as Dancer is imported to a script, that script becomes a webapp.  All
+the script has to do is to declare a list of B<routes>.  A route handler is
+composed by an HTTP method, a path pattern and a code block.
 
 The code block given to the route handler has to return a string which will be
 used as the content to render to the client.
@@ -210,6 +208,21 @@ be defined explicitly with the keyword 'r', like the following:
         my ($name) = splat;
         return "Hello $name";
     };
+
+=head2 RUNNING THE WEBSERVER
+
+Once the script is ready, you can run the webserver just by running the
+script. The following options are supported:
+
+=over 8
+
+=item B<--port=XXXX>    set the port to listen to (default is 1915)
+
+=item B<--daemon>       run the webserver in the background
+
+=item B<--help>         display a detailed help message
+
+=back
 
 =head1 ACTION SKIPPING
 
