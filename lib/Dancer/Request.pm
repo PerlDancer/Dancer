5f9536f6ee89b378878dacb45fac77eb08ab3837
Alexis Sukrieh <sukria@sukria.net>
2010-01-15 13:00:41 +0100

    first rewrite of Dancer::Request without CGI.pm
    It can build a request from the %ENV as described by the PSGI spec.
    Can also read GET/POST data itself.
    
    First test scripts.

--- lib/Dancer/Request.pm
+++ lib/Dancer/Request.pm
@@ -9,54 +9,162 @@ use warnings;
 use Dancer::SharedData;
 
 sub new {
-    my ($class, $request) = @_;
+    my ($class) = @_;
     my $self = {
-        path    => undef,
-        method  => undef,
-        request => $request || Dancer::SharedData->request,
+        path   => undef,
+        method => undef,
+        params => {},
+        _input => undef,
+        _chunk_size => 4096,
+        _raw_body => '',
+        _read_position => 0,
+        _content_length => $ENV{CONTENT_LENGTH},
     };
     bless $self, $class;
-    $self->init();
+    $self->_init();
     return $self;
+}
+
+# this is the way to ask for a hand-cooked request
+sub new_for_request {
+    my ($class, $method, $path, $params) = @_;
+    $params ||= {};
+
+    my $req = $class->new;
+    $req->{path}   = $path;
+    $req->{method} = $method;
+    $req->{params} = { %{$req->{params}}, %{$params} };
+
+    return $req;
 }
 
 # public interface (read-only)
 # use new to build a new request (may change)
-sub path   { $_[0]->{path}   }
-sub method { $_[0]->{method} }
+sub path           { $_[0]->{path}   }
+sub method         { $_[0]->{method} }
 sub request_method { shift->method }
-sub path_info { shift->path }
+sub path_info      { shift->path }
+sub input_handle   { shift->{_input} }
 
-# private
-sub init {
-    my ($self) = @_;
-    $self->build_path() unless $self->path;
-    $self->build_method() unless $self->method;
+sub params {
+    my ($self, $name) = @_;
+    return %{ $self->{params} } if wantarray && @_ == 1;
+    return $self->{params} if @_ == 1;
+    return $self->{params}{$name};
 }
 
-sub build_path {
-    my ($self) = @_;
+# private
 
+sub _init {
+    my ($self) = @_;
+    $self->_build_path() unless $self->path;
+    $self->_build_method() unless $self->method;
+    
+    # input for POST/PUT data are taken from PSGI if present, 
+    # fallback to STDIN
+    $self->{_input} = $ENV{'psgi.input'} ? $ENV{'psgi.input'} : *STDIN;
+    $self->_build_params();
+}
+
+sub _build_params {
+    my ($self) = @_;
+    my $params = {};
+
+    $self->_parse_get_params(\$params);
+    $self->_parse_post_params(\$params);
+    $self->{params} = $params;
+}
+
+# Written from PSGI specs:
+# http://search.cpan.org/dist/PSGI/PSGI.pod
+sub _build_path {
+    my ($self) = @_;
     my $path = "";
-    my $req = $self->{request};
-    if (defined $ENV{'SCRIPT_NAME'}) {
-        $path = $ENV{'SCRIPT_NAME'};
-        $path .= $ENV{'PATH_INFO'} if $ENV{'PATH_INFO'};
-    }
-    elsif (defined($req)) {
-        my $script_name = "";
-        eval { $script_name = $req->script_name }; 
-        $path .= $script_name if defined $script_name;
-        $path .= $req->path_info if defined $req->path_info;
-    }
+
+    $path .= $ENV{'SCRIPT_NAME'} 
+        if defined $ENV{'SCRIPT_NAME'};
+    $path .= $ENV{'PATH_INFO'} 
+        if defined $ENV{'PATH_INFO'};
+
+    # fallback to REQUEST_URI if nothing found
+    # we have to decode it, according to PSGI specs.
+    $path ||= $self->_url_decode($ENV{REQUEST_URI}) 
+        if defined $ENV{REQUEST_URI};
 
     die "Cannot resolve path" if not $path;
     $self->{path} = $path;
 }
 
-sub build_method {
+sub _build_method {
     my ($self) = @_;
     $self->{method} = $ENV{REQUEST_METHOD} || $self->{request}->request_method();
+}
+
+sub _url_decode {
+    my ($self, $encoded) = @_;
+    my $clean = $encoded;
+    $clean =~ tr/\+/ /;
+    $clean =~ s/%([a-fA-F0-9]{2})/pack "H2", $1/eg;
+    return $clean;
+}
+
+sub _parse_get_params {
+    my ($self, $r_params) = @_;
+    $self->_parse_params($r_params, $ENV{QUERY_STRING});
+}
+
+sub _parse_post_params {
+    my ($self, $r_params) = @_;
+    my $body = $self->_read_to_end();
+
+    $self->_parse_params($r_params, $body);
+}
+
+sub _parse_params {
+    my ($self, $r_params, $source) = @_;
+    return unless $source;
+    
+    foreach my $token (split /&/, $source) {
+        my ($key, $val) = split(/=/, $token);
+        $key = $self->_url_decode($key);
+        $val = $self->_url_decode($val);
+        $$r_params->{$key} = $val;
+    }
+    return $r_params;
+}
+
+# taken from Miyagawa's Plack::Request::BodyParser
+sub _read_to_end {
+    my ($self) = @_;
+    my $content_length = $ENV{CONTENT_LENGTH};
+ 
+    if ($content_length > 0) {
+        while (my $buffer = $self->_read() ) {
+            $self->{_raw_body} .= $buffer;
+        }
+    }
+    return $self->{_raw_body};
+}
+ 
+sub _read {
+    my ($self, ) = @_;
+    my $remaining = $ENV{CONTENT_LENGTH} - $self->{_read_position};
+    my $maxlength = $self->{_chunk_size};
+ 
+    return if ($remaining <= 0);
+ 
+    my $readlen = ($remaining > $maxlength) ? $maxlength : $remaining;
+    my $buffer;
+
+    my $rc = $self->input_handle->read($buffer, $readlen);
+ 
+    if (defined $rc) {
+        $self->{_read_position} += $rc;
+        return $buffer;
+    } 
+    else {
+        die "Unknown error reading input: $!";
+    }
 }
 
 1;
