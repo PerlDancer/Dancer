ca88a3f76f09f7a53a91e06e0f62e1c642a94b96
Alexis Sukrieh <sukria@sukria.net>
2010-01-22 14:55:53 +0100

    POD for Dancer::Template + autodetection of engines
    Added some documentation to Dancer::Template so the user
    can understand how to change a template engine.
    
    Refactor of the template engine loading so that any new
    template engine may be used without patching Dancer::Template.

--- lib/Dancer/Template.pm
+++ lib/Dancer/Template.pm
@@ -8,22 +8,88 @@ my $engine;
 sub engine { $engine }
 
 # init the engine according to the settings
+# the template engine module will take from the 
+# setting name.
 sub init {
     my ($self, $setting, $config) = @_;
+
+    # the default engine if no setting
     if ((not defined $setting) or ($setting eq 'simple')) {
         require Dancer::Template::Simple;
         return $engine = Dancer::Template::Simple->new;
     }
-    elsif ($setting eq 'template_toolkit') {
-        require Dancer::Template::TemplateToolkit;
-        return $engine = Dancer::Template::TemplateToolkit->new(settings => $config);
-    }elsif($setting eq 'micro_template') {
-        require Dancer::Template::MicroTemplate;
-        return $engine = Dancer::Template::MicroTemplate->new(settings => $config);
-    }else {
-        die "unknown template engine '$setting'";
-    }
+
+    # trying to load the engine
+    my $engine_class = _build_template_engine_from_setting($setting);
+    eval "require $engine_class";
+    die "unknown template engine '$setting'" if $@;
+
+    # creating the engine
+    $engine = $engine_class->new(settings => $config);
 }
+
+sub _build_template_engine_from_setting {
+    my ($setting) = @_;
+    my $module = "";
+    for my $token (split /_/, $setting) {
+        $module .= ucfirst($token);
+    }
+    return "Dancer::Template::${module}";
+}
+
 
 1;
 
+__END__
+=pod
+
+=head1 NAME
+
+Dancer::Template - template support in Dancer
+
+=head1 DESCRIPTION
+
+This module is the wrapper that provides support for different 
+template engines.
+
+=head1 USAGE
+
+=head2 Default engine
+
+The default engine used by Dancer::Template is Dancer::Template::Simple.
+If you want to change the engine used, you have to edit the B<template>
+configuration variable.
+
+=head2 Configuration
+
+The B<template> configuration variable tells Dancer which engine to use
+for rendering views.
+
+You change it either in your config.yml file:
+
+    # setting TT as the template engine
+    template: "template_toolkit" 
+
+Or in the application code:
+
+    # setting TT as the template engine
+    set template => 'template_toolkit';
+
+=head1 AUTHORS
+
+This module has been written by Alexis Sukrieh. See the AUTHORS file that comes
+with this distribution for details.
+
+=head1 LICENSE
+
+This module is free software and is released under the same terms as Perl
+itself.
+
+=head1 SEE ALSO
+
+See L<Dancer> for details about the complete framework.
+
+You can also search the CPAN for existing engines in the Dancer::Template
+namespace.
+
+=cut
