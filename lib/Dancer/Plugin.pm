ab7188656e6c15450488782c22623081da816a50
Alexis Sukrieh <sukria@sukria.net>
2010-04-03 18:50:32 +0200

    exports symbol plugin_setting in Dancer::Plugin

--- lib/Dancer/Plugin.pm
+++ lib/Dancer/Plugin.pm
@@ -2,17 +2,22 @@ package Dancer::Plugin;
 use strict;
 use warnings;
 
+use Dancer::Config;
+
 use base 'Exporter';
 use vars qw(@EXPORT);
 
 @EXPORT = qw(
     register
     register_plugin
+    plugin_setting
 );
 
 my @_reserved_keywords = @Dancer::EXPORT;
 
 my $_keywords = [];
+
+sub plugin_setting { Dancer::Config::plugin_setting(@_) }
 
 sub register($$) {
     my ($keyword, $code) = @_;
