a7c73ff61785349de53f33a51db8a4875bb48ee6
franck cuny <franck@lumberjaph.net>
2010-04-05 12:18:33 +0200

    use charset from settings

--- lib/Dancer/Renderer.pm
+++ lib/Dancer/Renderer.pm
@@ -162,13 +162,14 @@ sub get_mime_type {
 
 # set of builtin templates needed by Dancer when rendering HTML pages
 sub templates {
+    my $charset = setting('charset') || 'UTF-8';
     {   default =>
           '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
 <head>
 <title><% title %></title>
 <link rel="stylesheet" type="text/css" href="/css/<% style %>.css" />
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset='.$charset.'" />
 </head>
 <body>
 <h1><% title %></h1>
