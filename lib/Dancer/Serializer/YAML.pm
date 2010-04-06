c131d8de264ebcee8b2ff019efb1ddfc8ec712e4
franck cuny <franck@lumberjaph.net>
2010-04-06 22:04:25 +0200

    as for JSON

--- lib/Dancer/Serializer/YAML.pm
+++ lib/Dancer/Serializer/YAML.pm
@@ -12,14 +12,41 @@ sub init {
 
 sub serialize {
     my ($self, $entity) = @_;
-    YAML::Dump $entity;
+    YAML::Dump($entity);
 }
 
 sub deserialize {
     my ($self, $content) = @_;
-    YAML::Load $content;
+    YAML::Load($content);
 }
 
 sub content_type { 'text/x-yaml' }
 
 1;
+__END__
+
+=head1 NAME
+
+Dancer::Serializer::YAML
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head2 METHODS
+
+=over 4
+
+=item B<serialize>
+
+Serialize a data structure to a YAML structure.
+
+=item B<deserialize>
+
+Deserialize a YAML structure to a data structure
+
+=item B<content_type>
+
+Return 'application/json'
+
+=back
