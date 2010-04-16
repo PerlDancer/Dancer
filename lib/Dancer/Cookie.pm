70b3af4e78771404f531236aa8b980cbf6fa21be
sawyer <sawyer@flore.(none)>
2010-04-16 22:36:12 +0300

    documenting Dancer::Cookie

--- lib/Dancer/Cookie.pm
+++ lib/Dancer/Cookie.pm
@@ -40,3 +40,80 @@ sub _epoch_to_gmtstring {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Dancer::Cookie - A Dancer cookie object
+
+=head1 SYNOPSIS
+
+    use Dancer::Cookie;
+
+    my $cookie = Dancer::Cookie->new(
+        name => $cookie_name, value => $cookie_value
+    );
+
+=head1 DESCRIPTION
+
+Dancer::Cookie provides a HTTP cookie object to work with cookies.
+
+=head1 ATTRIBUTES
+
+=head3 name
+
+The cookie's name.
+
+=head3 value
+
+The cookie's value.
+
+=head2 expires
+
+The cookie's expiration date.
+
+=head2 domain
+
+The cookie's domain.
+
+=head2 path
+
+The cookie's path.
+
+=head1 METHODS/SUBROUTINES
+
+=head2 new
+
+Create a new Dancer::Cookie object.
+
+You can set any attribute described in the I<ATTRIBUTES> section above.
+
+=head2 init
+
+Runs an expiration test and sets a default path if not set.
+
+=head2 to_header
+
+Creates a proper HTTP cookie header from the content.
+
+=head2 _epoch_to_gmtstring
+
+Internal method to convert the time from Epoch to GMT.
+
+=head1 AUTHOR
+
+Alexis Sukrieh
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2009-2010 Alexis Sukrieh.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
