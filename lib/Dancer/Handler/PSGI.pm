2151f889449352b907cdc400bf918f7b442ca181
Alexis Sukrieh <sukria@sukria.net>
2010-04-21 15:17:07 +0200

    Plack::Request should be loaded dynamically

--- lib/Dancer/Handler/PSGI.pm
+++ lib/Dancer/Handler/PSGI.pm
@@ -4,14 +4,18 @@ use strict;
 use warnings;
 use base 'Dancer::Handler';
 
-use Plack::Request;
 use Dancer::GetOpt;
 use Dancer::Headers;
 use Dancer::Config;
+use Dancer::ModuleLoader;
 use Dancer::SharedData;
 
 sub new {
     my $class = shift;
+
+    die "Plack::Request is needed by the PSGI handler"
+        unless Dancer::ModuleLoader->load('Plack::Request');
+
     my $self  = {};
     bless $self, $class;
     Dancer::Route->init();
