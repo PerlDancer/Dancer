7cc7a781b1bf1c72ff8ad73960cb8b1dc5ab6791
Alexis Sukrieh <sukria@sukria.net>
2010-06-14 14:19:40 +0200

    FIX for bug RT#57805
    Dancer::Serializer::Abstract now parses correctly the Content-Type
    header of incoming requests.

--- lib/Dancer/Serializer/Abstract.pm
+++ lib/Dancer/Serializer/Abstract.pm
@@ -18,6 +18,8 @@ sub content_type { 'text/plain' }
 # most serializer don't have to overload this one
 sub support_content_type {
     my ($self, $ct) = @_;
+    my @toks = split ';', $ct;
+    $ct = lc($toks[0]);
     return $ct eq $self->content_type;
 }
 
