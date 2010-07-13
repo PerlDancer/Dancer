0bdb41f6a37bb5a598442296238fd24baf424186
Alexis Sukrieh <sukria@sukria.net>
2010-07-13 09:56:54 +0200

    add is_head accessor in Dancer::Request

--- lib/Dancer/Request.pm
+++ lib/Dancer/Request.pm
@@ -32,6 +32,7 @@ Dancer::Request->attributes(
 sub agent                 { $_[0]->user_agent }
 sub remote_address        { $_[0]->{env}->{'REMOTE_ADDR'} }
 sub forwarded_for_address { $_[0]->{env}->{'X_FORWARDED_FOR'} }
+sub is_head               { $_[0]->{method} eq 'HEAD' }
 sub is_post               { $_[0]->{method} eq 'POST' }
 sub is_get                { $_[0]->{method} eq 'GET' }
 sub is_put                { $_[0]->{method} eq 'PUT' }
