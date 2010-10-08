2dcfec768d4de3735e567283116049f4967a70ad
Sawyer X <xsawyerx@cpan.org>
2010-10-08 22:13:48 +0200

    documentation for Dancer::Logger::Console

--- lib/Dancer/Logger/Console.pm
+++ lib/Dancer/Logger/Console.pm
@@ -9,3 +9,36 @@ sub _log {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Dancer::Logger::Console - console-based logging engine for Dancer
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This is a console-based logging engine that prints your logs to the console.
+
+=head1 METHODS
+
+=head2 _log
+
+Writes the log message to the console/screen.
+
+=head1 AUTHOR
+
+Alexis Sukrieh
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2009-2010 Alexis Sukrieh.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
