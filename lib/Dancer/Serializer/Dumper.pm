56d4a5f1227ad3d7a58570d8b1a7516ae6433e4f
Sawyer X <xsawyerx@cpan.org>
2010-11-03 22:34:19 +0200

    adding Dancer::Serializer::Dumper documentation

--- lib/Dancer/Serializer/Dumper.pm
+++ lib/Dancer/Serializer/Dumper.pm
@@ -39,3 +39,45 @@ sub deserialize {
 sub content_type {'text/x-data-dumper'}
 
 1;
+
+__END__
+
+=head1 NAME
+
+Dancer::Serializer::Dumper - Data::Dumper serializer
+
+=head1 SYNOPSIS
+
+    use Dancer::Serializer::Dumper;
+
+    my $serializer = Data::Serializer::Dumper->new;
+    my $dump       = $serializer->serialize( { name => 'John Doe' } );
+    my $hash       = $serializer->deserialize($dump);
+
+=head1 DESCRIPTION
+
+This engine allows you to serialize your data to L<Data::Dumper> and deserialize
+from L<Data::Dumper>.
+
+You can configure it in your C<config.yml> file:
+
+    serializer: Dumper
+
+or directly in your app code with the B<set> keyword.
+
+    set serializer => 'Dumper';
+
+=head2 METHODS
+
+=head3 serialize
+
+Serialize a data structure to a Data::Dumper structure.
+
+=head3 deserialize
+
+Deserialize a Data::Dumper structure to a data structure.
+
+=head3 content_type
+
+Return 'text/x-data-dumper'
+
