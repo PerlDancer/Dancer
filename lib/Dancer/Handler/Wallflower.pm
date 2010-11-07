a959e89edfa185880719f162b86a536fa85c6f65
Philippe Bruhat (BooK) <book@cpan.org>
2010-11-07 01:15:10 +0100

    save response content to a file

--- lib/Dancer/Handler/Wallflower.pm
+++ lib/Dancer/Handler/Wallflower.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use base 'Dancer::Handler';
 
-use File::Path;
+use File::Path 'mkpath';
 
 sub new { return bless {}, __PACKAGE__ }
 
@@ -24,17 +24,17 @@ sub dance {
         next if /^\s*(#|$)/;
         chomp;
 
-        # strip query string and fragment
+        # strip everything but the path
         my $url = URI->new(URI->new($_)->path);
 
         # default values
         my ($status, $file, $bytes) = (500, '-', '-');
         $log = "$status $url";
 
-        # skip bad URL
-        next if !/^\//;
+        # require an absolute path
+        next if $url !~ /\//;
 
-        # fake an environment
+        # fake a minimal environment
         local $ENV{SERVER_NAME} = 'wallflower';
         local $ENV{SERVER_PORT} = '80';
 
@@ -50,6 +50,40 @@ sub dance {
 
         # save successes to the appropriate file
         if ($status eq '200') {
+
+            # absolute paths have the empty string as their first path_segment
+            my (undef, @segments) = $url->path_segments;
+
+            # assume directory
+            push @segments, $index if $segments[-1] !~ /\./;
+
+            # generate target file name
+            my $file = File::Spec->catfile($wwwdocs, @segments);
+            pop @segments;
+            my $dir = File::Spec->catdir($wwwdocs, @segments);
+
+            # ensure the subdirectory exists
+            mkpath $dir if !-e $dir;
+            open my $fh, '>', $file or die "Can't open $file for writing: $!";
+
+            # copy content to the file
+            if (ref $content eq 'ARRAY') {
+                print $fh @$content;
+            }
+            elsif (ref $content eq 'GLOB') {
+                print {$fh} <$content>;
+            }
+            elsif (eval { $content->can('getlines') }) {
+                print {$fh} $content->getlines;
+            }
+            else {
+                die "Don't know how to handle $content";
+            }
+
+            # finish
+            close $fh;
+            $bytes = -s $file;
+            $log .= " => $file [$bytes]";
         }
     }
     continue {
