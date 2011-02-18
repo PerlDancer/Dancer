1db61b15cdf74bcdf99471b86a97c7641ee350b3
franck cuny <franck@lumberjaph.net>
2011-02-18 17:54:52 +0100

    remove deprecated 'r' sub; add deprecation message

--- lib/Dancer.pm
+++ lib/Dancer.pm
@@ -138,7 +138,6 @@ sub prefix          { Dancer::App->current->set_prefix(@_) }
 sub del             { Dancer::App->current->registry->universal_add('delete',  @_) }
 sub options         { Dancer::App->current->registry->universal_add('options', @_) }
 sub put             { Dancer::App->current->registry->universal_add('put',     @_) }
-sub r               { croak "'r' is DEPRECATED, use qr{} instead" }
 sub redirect  {
     my ($destination, $status) = @_;
     if ($destination =~ m!^(\w://)?/!) {
@@ -152,8 +151,12 @@ sub redirect  {
 }
 sub render_with_layout {
     my ($content, $tokens, $options) = @_;
-    carp "'render_with_layout' is DEPRECATED, use the 'engine' keyword "
-       . "to get the template engine, and use 'apply_layout' on the result";
+
+    Dancer::Deprecation::deprecated(
+        feature => 'render_with_layout',
+        version => '1.3000',
+        reason  => "use the 'engine' keyword to get the template engine, and use 'apply_layout' on the result",
+    );
 
     my $full_content = Dancer::Template->engine->apply_layout($content, $tokens, $options);
 
@@ -778,21 +781,6 @@ Defines a route for HTTP B<OPTIONS> requests to the given URL:
 Defines a route for HTTP B<PUT> requests to the given URL:
 
     put '/resource' => sub { ... };
-
-=head2 r
-
-Defines a route pattern as a regular Perl regexp.
-
-This method is B<DEPRECATED>. Dancer now supports real Perl Regexp objects
-instead. You should not use r() but qr{} instead:
-
-Don't do this:
-
-    get r('/some/pattern(.*)') => sub { };
-
-But rather this:
-
-    get qr{/some/pattern(.*)} => sub { };
 
 =head2 redirect
 
