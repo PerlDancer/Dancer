d8e79e0d63d0e1b0e05fd36f9e31c378678fccc3
Sawyer X <xsawyerx@cpan.org>
2011-02-20 17:59:57 +0200

    avoid possible loss of last directory

--- lib/Dancer.pm
+++ lib/Dancer.pm
@@ -312,6 +312,13 @@ sub _init {
     
     my ($script_vol, $script_dirs, $script_name) =
       File::Spec->splitpath(File::Spec->rel2abs($script));
+
+    # normalize
+    if ( -d ( my $fulldir = File::Spec->catdir( $script_dirs, $script_name ) ) ) {
+        $script_dirs = $fulldir;
+        $script_name = '';
+    }
+
     my @script_dirs = File::Spec->splitdir($script_dirs);
     my $script_path = Dancer::FileUtils::d_catdir($script_vol, $script_dirs);
 
