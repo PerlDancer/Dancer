67af6bde2955941c21c6c8738f21362ea0ffc1c2
franck cuny <franck@lumberjaph.net>
2011-03-06 17:56:44 +0100

    start to add documentation
    Signed-off-by: franck cuny <franck@lumberjaph.net>

--- lib/Dancer/Hook.pm
+++ lib/Dancer/Hook.pm
@@ -135,17 +135,42 @@ Manipulate hooks with Dancer
 
 =head1 METHODS
 
-=head2 register_hook
+=head2 register_hook ($hook_name, [$properties], $code)
+
+    hook 'before', {apps => ['main']}, sub {...};
+
+    hook 'before' => sub {...};
+    
+Attaches a hook at some point, with a possible list of properties.
+
+Currently supported properties:
+
+=over 4
+
+=item apps
+
+    an array reference containing apps name
+
+=back
 
 =head2 register_hooks_name
 
-=head2 get_hooks_name_list
+Add a new hook name, so developpers of application can insert some code at this point.
+
+    package My::Dancer::Plugin;
+    Dancer::Hook->instance->register_hooks_name(qw/before_auth after_auth/);
 
 =head2 hook_is_registered
 
+Test if a hook with this name has already been registered.
+
 =head2 execute_hooks
 
+Execute a list of hooks for some position    
+
 =head2 get_hooks_for
+
+Returns the list of coderef registered for a given position
 
 =head1 AUTHORS
 
