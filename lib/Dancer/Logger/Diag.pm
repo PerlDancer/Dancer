defd8f065e4d8a6fa4ea026d6c905d6676710ad6
ambs <ambs@cpan.org>
2011-06-11 17:55:43 +0100

    Weaved Logger::Diag

--- lib/Dancer/Logger/Diag.pm
+++ lib/Dancer/Logger/Diag.pm
@@ -1,13 +1,43 @@
 package Dancer::Logger::Diag;
+# ABSTRACT: Test::More diag() logging engine for Dancer
+
+=head1 SYNOPSIS
+
+  my $diag = Dancer::Logger::Diag->new();
+  $diag->_log( $level, $message);
+
+=head1 DESCRIPTION
+
+This logging engine uses L<Test::More>'s diag() to output as TAP comments.
+
+This is very useful in case you're writing a test and want to have logging
+messages as part of your TAP.
+
+=cut
+
 use strict;
 use warnings;
 use base 'Dancer::Logger::Abstract';
+
+
+=method init
+
+This method is called when C<< ->new() >> is called. It just loads
+Test::More lazily.
+
+=cut
 
 sub init {
     my $self = shift;
     $self->SUPER::init(@_);
     require Test::More;
 }
+
+=method _log
+
+Use Test::More's diag() to output the log message.
+
+=cut
 
 sub _log {
     my ($self, $level, $message) = @_;
@@ -18,44 +48,4 @@ sub _log {
 }
 
 1;
-
-__END__
-
-=head1 NAME
-
-Dancer::Logger::Diag - Test::More diag() logging engine for Dancer
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-This logging engine uses L<Test::More>'s diag() to output as TAP comments.
-
-This is very useful in case you're writing a test and want to have logging
-messages as part of your TAP.
-
-=head1 METHODS
-
-=head2 init
-
-This method is called when C<< ->new() >> is called. It just loads Test::More
-lazily.
-
-=head2 _log
-
-Use Test::More's diag() to output the log message.
-
-=head1 AUTHOR
-
-Alexis Sukrieh
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2009-2010 Alexis Sukrieh.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
 
