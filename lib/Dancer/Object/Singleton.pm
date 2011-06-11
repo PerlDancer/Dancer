5828dd81f93bdfcb0a039676b68257cd44b13035
ambs <ambs@cpan.org>
2011-06-11 17:43:55 +0100

    Weaved Object::Singleton

--- lib/Dancer/Object/Singleton.pm
+++ lib/Dancer/Object/Singleton.pm
@@ -1,69 +1,5 @@
 package Dancer::Object::Singleton;
-
-# This class is a root class for singleton objects in Dancer.
-# It provides basic OO singleton tools for Perl5 without being... MooseX::Singleton ;-)
-
-use strict;
-use warnings;
-use Carp;
-
-use base qw(Dancer::Object);
-
-# pool of instances (only one per package name)
-my %instances;
-
-# constructor
-sub new {
-    my ($class) = @_;
-    croak "you can't call 'new' on $class, as it's a singleton. Try to call 'instance'";
-}
-
-sub clone {
-    my ($class) = @_;
-    croak "you can't call 'clone' on $class, as it's a singleton. Try to call 'instance'";
-}
-
-sub instance {
-    my ($class) = @_;
-    my $instance = $instances{$class};
-
-    # if exists already
-    defined $instance
-      and return $instance;
-
-    # create the instance
-    $instance = bless {}, $class;
-    $class->init($instance);
-
-    # save and return it
-    $instances{$class} = $instance;
-    return $instance;
-}
-
-# accessor code for singleton objects
-# (overloaded from Dancer::Object)
-sub _setter_code {
-    my ($class, $attr) = @_;
-    sub {
-        my ($class_or_instance, $value) = @_;
-        my $instance = ref $class_or_instance ?
-          $class_or_instance : $class_or_instance->instance;
-        if (@_ == 1) {
-            return $instance->{$attr};
-        }
-        else {
-            return $instance->{$attr} = $value;
-        }
-    };
-}
-
-1;
-
-__END__
-
-=head1 NAME
-
-Dancer::Object::Singleton - Singleton base class for Dancer
+# ABSTRACT: Singleton base class for Dancer
 
 =head1 SYNOPSIS
 
@@ -93,39 +29,90 @@ in the application.
 
 It provides you with attributes and an initializer.
 
-=head1 METHODS
+=cut
 
-=head2 instance
+use strict;
+use warnings;
+use Carp;
 
-Returns the instance of the singleton. The instance is created only when
-needed. The creation will call the C<init()> method, which you should implement.
-
-=head2 init
-
-Exists but does nothing. This is so you won't have to write an initializer if
-you don't want to. init receives the instance as argument.
-
-=head2 get_attributes
-
-Get the attributes of the specific class.
-
-=head2 attributes
+=method attributes
 
 Generates attributes for whatever object is extending Dancer::Object and saves
 them in an internal hashref so they can be later fetched using
 C<get_attributes>.
 
-=head1 AUTHOR
 
-Damien Krotkine
+=method get_attributes
 
-=head1 LICENSE AND COPYRIGHT
+Get the attributes of the specific class.
 
-Copyright 2010 Damien Krotkine.
+=method init
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
+Exists but does nothing. This is so you won't have to write an initializer if
+you don't want to. init receives the instance as argument.
+=cut
 
-See http://dev.perl.org/licenses/ for more information.
+
+use base qw(Dancer::Object);
+
+# pool of instances (only one per package name)
+my %instances;
+
+# constructor
+sub new {
+    my ($class) = @_;
+    croak "you can't call 'new' on $class, as it's a singleton. Try to call 'instance'";
+}
+
+sub clone {
+    my ($class) = @_;
+    croak "you can't call 'clone' on $class, as it's a singleton. Try to call 'instance'";
+}
+
+
+=method instance
+
+Returns the instance of the singleton. The instance is created only when
+needed. The creation will call the C<init()> method, which you should implement.
+
+=cut
+
+sub instance {
+    my ($class) = @_;
+    my $instance = $instances{$class};
+
+    # if exists already
+    defined $instance
+      and return $instance;
+
+    # create the instance
+    $instance = bless {}, $class;
+    $class->init($instance);
+
+    # save and return it
+    $instances{$class} = $instance;
+    return $instance;
+}
+
+
+# private
+
+# accessor code for singleton objects
+# (overloaded from Dancer::Object)
+sub _setter_code {
+    my ($class, $attr) = @_;
+    sub {
+        my ($class_or_instance, $value) = @_;
+        my $instance = ref $class_or_instance ?
+          $class_or_instance : $class_or_instance->instance;
+        if (@_ == 1) {
+            return $instance->{$attr};
+        }
+        else {
+            return $instance->{$attr} = $value;
+        }
+    };
+}
+
+1;
 
