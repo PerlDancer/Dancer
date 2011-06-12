a10c34fcab293bc4abdf91b7a0cccd328c3786f6
ambs <ambs@cpan.org>
2011-06-12 14:33:01 +0100

    Two more

--- lib/Dancer/Logger/Null.pm
+++ lib/Dancer/Logger/Null.pm
@@ -1,17 +1,5 @@
 package Dancer::Logger::Null;
-use strict;
-use warnings;
-use base 'Dancer::Logger::Abstract';
-
-sub _log {1}
-
-1;
-
-__END__
-
-=head1 NAME
-
-Dancer::Logger::Null - blackhole-like silent logging engine for Dancer
+# ABSTRACT: blackhole-like silent logging engine for Dancer
 
 =head1 SYNOPSIS
 
@@ -20,23 +8,20 @@ Dancer::Logger::Null - blackhole-like silent logging engine for Dancer
 This logger acts as a blackhole (or /dev/null, if you will) that discards all
 the log messages instead of displaying them anywhere.
 
-=head1 METHODS
+=cut
 
-=head2 _log
+use strict;
+use warnings;
+use base 'Dancer::Logger::Abstract';
+
+=method _log
 
 Discards the message.
 
-=head1 AUTHOR
+=cut
+sub _log {1}
 
-Alexis Sukrieh
+1;
 
-=head1 LICENSE AND COPYRIGHT
 
-Copyright 2009-2010 Alexis Sukrieh.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
 
