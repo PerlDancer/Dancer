33e3213289faae692a0d9431a1e90f04f1ea5e66
ambs <ambs@cpan.org>
2011-06-12 14:45:55 +0100

    Two more

--- lib/Dancer/Template/Abstract.pm
+++ lib/Dancer/Template/Abstract.pm
@@ -1,4 +1,12 @@
 package Dancer::Template::Abstract;
+# ABSTRACT: abstract class for Dancer's template engines
+
+=head1 DESCRIPTION
+
+This class is provided as a base class for each template engine. Any template
+engine must inherit from it and provide a set of methods described below.
+
+=cut
 
 use strict;
 use warnings;
@@ -10,24 +18,82 @@ use Dancer::FileUtils 'path';
 
 use base 'Dancer::Engine';
 
+
+=method init()
+
+The template engine can overload this method if some initialization
+stuff has to be done before the template engine is used.
+
+The base class provides a plain init() method that only returns true.
+
+=cut
+
+
 Dancer::Factory::Hook->instance->install_hooks(
     qw/before_template_render after_template_render before_layout_render after_layout_render/
 );
 
-# overloads this method to implement the rendering
-# args:   $self, $template, $tokens
-# return: a string of $template's content processed with $tokens
+=method render($self, $template, $tokens)
+
+This method must be implemented by the template engine. Given a template and a
+set of tokens, it returns a processed string.
+
+If C<$template> is a reference, it's assumed to be a reference to a string that
+contains the template itself. If it's not a reference, it's assumed to be the
+path to template file, as a string. The render method will then have to open it
+and read its content (Dancer::FileUtils::read_file_content does that job).
+
+This method's return value must be a string which is the result of the
+interpolation of C<$tokens> in C<$template>.
+
+If an error occurs, the method should trigger an exception with C<die()>.
+
+Examples :
+
+    # with a template as a file
+    $content = $engine->render('/my/template.txt', { var => 42 };
+
+    # with a template as a scalar
+    my $template = "here is <% var %>";
+    $content = $engine->render(\$template, { var => 42 });
+
+=back
+
+=cut
 sub render { confess "render not implemented" }
 
+
+=method default_tmpl_ext()
+
+Template class that inherits this class should override this method to
+return a default template extension, example: for Template::Toolkit it
+returns "tt" and for HTML::Mason it returns "mason".  So when you call
+C<template 'index';> in your dispatch code, Dancer will look for a
+file 'index.tt' or 'index.mason' based on the template you use.
+
+B<Note 1:> when returning the extension string, please do not add a
+dot in front of the extension as Dancer will do that.
+
+B<Note 2:> for backwards compatibility abstract class returns "tt"
+instead of throwing an exception 'method not implemented'.
+
+User would be able to change the default extension using the
+C<<extension>> configuration variable on the template
+configuration. For example, for the default (C<Simple>) engine:
+
+     template: "simple"
+     engines:
+       simple:
+         extension: 'tmpl'
+
+=cut
 sub default_tmpl_ext { "tt" }
 
-sub _template_name {
-    my ( $self, $view ) = @_;
-    my $def_tmpl_ext = $self->config->{extension} || $self->default_tmpl_ext();
-    $view .= ".$def_tmpl_ext" if $view !~ /\.\Q$def_tmpl_ext\E$/;
-    return $view;
-}
+=method view($view)
 
+The default behavior of this method is to return the path of the given view.
+
+=cut
 sub view {
     my ($self, $view) = @_;
 
@@ -36,6 +102,11 @@ sub view {
     return path(Dancer::App->current->setting('views'), $view);
 }
 
+=method layout($layout, $tokens, $content)
+
+The default behavior of this method is to merge a content with a layout.
+
+=cut
 sub layout {
     my ($self, $layout, $tokens, $content) = @_;
 
@@ -153,89 +224,13 @@ sub template {
     )->render();
 }
 
+# private
+
+sub _template_name {
+    my ( $self, $view ) = @_;
+    my $def_tmpl_ext = $self->config->{extension} || $self->default_tmpl_ext();
+    $view .= ".$def_tmpl_ext" if $view !~ /\.\Q$def_tmpl_ext\E$/;
+    return $view;
+}
+
 1;
-__END__
-
-=pod
-
-=head1 NAME
-
-Dancer::Template::Abstract - abstract class for Dancer's template engines
-
-=head1 DESCRIPTION
-
-This class is provided as a base class for each template engine. Any template
-engine must inherit from it and provide a set of methods described below.
-
-=head1 INTERFACE
-
-=over 4
-
-=item B<init()>
-
-The template engine can overload this method if some initialization stuff has to
-be done before the template engine is used.
-
-The base class provides a plain init() method that only returns true.
-
-=item B<default_tmpl_ext()>
-
-Template class that inherits this class should override this method to return a default template
-extension, example: for Template::Toolkit it returns "tt" and for HTML::Mason it returns "mason".
-So when you call C<template 'index';> in your dispatch code, Dancer will look for a file 'index.tt'
-or 'index.mason' based on the template you use.
-
-Note 1: when returning the extension string, please do not add a dot in front of the extension
-as Dancer will do that.
-
-Note 2: for backwards compatibility abstract class returns "tt" instead of throwing
-an exception 'method not implemented'.
-
-User would be able to change the default extension using the
-C<<extension>> configuration variable on the template
-configuration. For example, for the default (C<Simple>) engine:
-
-     template: "simple"
-     engines:
-       simple:
-         extension: 'tmpl'
-
-=item B<view($view)>
-
-The default behavior of this method is to return the path of the given view.
-
-=item B<layout($layout, $tokens, $content)>
-
-The default behavior of this method is to merge a content with a layout.
-
-=item B<render($self, $template, $tokens)>
-
-This method must be implemented by the template engine. Given a template and a
-set of tokens, it returns a processed string.
-
-If C<$template> is a reference, it's assumed to be a reference to a string that
-contains the template itself. If it's not a reference, it's assumed to be the
-path to template file, as a string. The render method will then have to open it
-and read its content (Dancer::FileUtils::read_file_content does that job).
-
-This method's return value must be a string which is the result of the
-interpolation of C<$tokens> in C<$template>.
-
-If an error occurs, the method should trigger an exception with C<die()>.
-
-Examples :
-
-    # with a template as a file
-    $content = $engine->render('/my/template.txt', { var => 42 };
-
-    # with a template as a scalar
-    my $template = "here is <% var %>";
-    $content = $engine->render(\$template, { var => 42 });
-
-=back
-
-=head1 AUTHOR
-
-This module has been written by Alexis Sukrieh, see L<Dancer> for details.
-
-=cut
