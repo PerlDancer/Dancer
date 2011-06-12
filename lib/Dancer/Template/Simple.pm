bfdf21ecd365712a1970612f255fac0782070b02
ambs <ambs@cpan.org>
2011-06-12 14:33:01 +0100

    Two more

--- lib/Dancer/Template/Simple.pm
+++ lib/Dancer/Template/Simple.pm
@@ -1,4 +1,37 @@
 package Dancer::Template::Simple;
+# ABSTRACT: pure Perl 5 template engine for Dancer
+
+=head1 DESCRIPTION
+
+This template engine is provided as a default one for the Dancer micro
+framework.
+
+This template engine should be fine for development purposes but is not a
+powerful one, it's written in pure Perl and has no C bindings to accelerate the
+template processing.
+
+If you want to power an application with Dancer in production environment, it's
+strongly advised to switch to Dancer::Template::TemplateToolkit.
+
+=head1 SYNTAX
+
+A template written for Dancer::Template::Simple should be working just fine with
+Dancer::Template::TemplateToolkit. The opposite is not true though.
+
+=over 4
+
+=item B<variables>
+
+To interpolate a variable in the template, use the following syntax:
+
+    <% var1 %>
+
+If 'var1' exists in the tokens hash given, its value will be written there.
+
+=back
+
+=cut
+
 use strict;
 use warnings;
 use Carp;
@@ -153,54 +186,5 @@ sub _interpolate_value {
 
 1;
 
-__END__
 
-=pod
 
-=head1 NAME
-
-Dancer::Template::Simple - pure Perl 5 template engine for Dancer
-
-=head1 DESCRIPTION
-
-This template engine is provided as a default one for the Dancer micro
-framework.
-
-This template engine should be fine for development purposes but is not a
-powerful one, it's written in pure Perl and has no C bindings to accelerate the
-template processing.
-
-If you want to power an application with Dancer in production environment, it's
-strongly advised to switch to Dancer::Template::TemplateToolkit.
-
-=head1 SYNTAX
-
-A template written for Dancer::Template::Simple should be working just fine with
-Dancer::Template::TemplateToolkit. The opposite is not true though.
-
-=over 4
-
-=item B<variables>
-
-To interpolate a variable in the template, use the following syntax:
-
-    <% var1 %>
-
-If 'var1' exists in the tokens hash given, its value will be written there.
-
-=back
-
-=head1 SEE ALSO
-
-L<Dancer>, L<Dancer::Template>
-
-=head1 AUTHOR
-
-This module has been written by Alexis Sukrieh.
-
-=head1 LICENSE
-
-This module is free software and is released under the same terms as Perl
-itself.
-
-=cut
