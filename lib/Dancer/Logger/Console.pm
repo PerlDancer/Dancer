811d3d286adbaf4f1b91d9a4eb0c38122fe72d97
ambs <ambs@cpan.org>
2011-06-12 14:50:22 +0100

    Some More

--- lib/Dancer/Logger/Console.pm
+++ lib/Dancer/Logger/Console.pm
@@ -1,20 +1,5 @@
 package Dancer::Logger::Console;
-use strict;
-use warnings;
-use base 'Dancer::Logger::Abstract';
-
-sub _log {
-    my ($self, $level, $message) = @_;
-    print STDERR $self->format_message($level => $message);
-}
-
-1;
-
-__END__
-
-=head1 NAME
-
-Dancer::Logger::Console - console-based logging engine for Dancer
+# ABSTRACT: console-based logging engine for Dancer
 
 =head1 SYNOPSIS
 
@@ -22,23 +7,20 @@ Dancer::Logger::Console - console-based logging engine for Dancer
 
 This is a console-based logging engine that prints your logs to the console.
 
-=head1 METHODS
+=cut
+use strict;
+use warnings;
+use base 'Dancer::Logger::Abstract';
 
-=head2 _log
+=method _log
 
 Writes the log message to the console/screen.
 
-=head1 AUTHOR
+=cut
+sub _log {
+    my ($self, $level, $message) = @_;
+    print STDERR $self->format_message($level => $message);
+}
 
-Alexis Sukrieh
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2009-2010 Alexis Sukrieh.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
+1;
 
