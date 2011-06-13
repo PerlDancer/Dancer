8e847e39adbfc5ef5625716e7e4428415b5a6b72
ambs <ambs@cpan.org>
2011-06-13 18:33:42 +0100

    Docs for Serializer::Dumper

--- lib/Dancer/Serializer/Dumper.pm
+++ lib/Dancer/Serializer/Dumper.pm
@@ -1,5 +1,11 @@
 package Dancer::Serializer::Dumper;
-# ABSTRACT: Data::Sumper serializer engine 
+# ABSTRACT: Data::Sumper serializer engine
+
+=head1 ABSTRACT
+
+Perl dumper format serializer.
+
+=cut
 
 use strict;
 use warnings;
@@ -7,18 +13,34 @@ use Carp;
 use base 'Dancer::Serializer::Abstract';
 use Data::Dumper;
 
+=method from_dumper
+
+Subroutine that converts a Dumper string in the respective data
+structure.
+
+=cut
 sub from_dumper {
     my ($string) = @_;
     my $s = Dancer::Serializer::Dumper->new;
     $s->deserialize($string);
 }
 
+=method to_dumper
+
+Subroutine that converts a data structure in a Dumper string.
+
+=cut
 sub to_dumper {
     my ($data) = @_;
     my $s = Dancer::Serializer::Dumper->new;
     $s->serialize($data);
 }
 
+=method serialize
+
+Method that converts a data structure in a Dumper string.
+
+=cut
 sub serialize {
     my ($self, $entity) = @_;
     {
@@ -27,6 +49,11 @@ sub serialize {
     }
 }
 
+=method deserialize
+
+Method that converts a Dumper string in a Perl data structure.
+
+=cut
 sub deserialize {
     my ($self, $content) = @_;
     my $res = eval "my \$VAR1; $content";
@@ -34,6 +61,11 @@ sub deserialize {
     return $res;
 }
 
+=method content_type
+
+Returns Perl data dumper content type string.
+
+=cut
 sub content_type {'text/x-data-dumper'}
 
 1;
