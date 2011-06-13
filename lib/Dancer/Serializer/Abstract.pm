d58b864ad8e4dd38e5ae3b93875e9cc3bb900782
ambs <ambs@cpan.org>
2011-06-13 18:27:17 +0100

    Documented Serializer/Abstract

--- lib/Dancer/Serializer/Abstract.pm
+++ lib/Dancer/Serializer/Abstract.pm
@@ -1,23 +1,60 @@
 package Dancer::Serializer::Abstract;
 # ABSTRACT: interface for serializer engines
 
+=head1 DESCRIPTION
+
+Abstract class for all serializer engines.
+
+=cut
+
 use strict;
 use warnings;
 use Carp;
 use base 'Dancer::Engine';
 
+=method serialize
+
+This method that all serializers should implement receive a reference
+to a Perl structure and should return a string with the serialized
+data.
+
+=cut
 sub serialize   { confess 'must be implemented' }
+
+=method deserialize
+
+This method that all serializers should implement receive a string
+with some serialized data, and should return a reference to a Perl
+structure that represents that data.
+
+=cut
 sub deserialize { confess 'must be implemented' }
 
-# must be implemented to delcare if the serializer can be used or not
-# most of the time, just use :
-# Dancer::ModuleLoader->load('Your::Serializer::Deps');
-sub loaded {0}
+=method loaded
 
-# should be implemented, fallback to text/plain if not
+Must be implemented to delcare if the serializer can be used or not
+most of the time, just use:
+
+   Dancer::ModuleLoader->load('Your::Serializer::Deps');
+
+=cut
+sub loaded { 0 }
+
+=method content_type
+
+Should be implemented, returning the content type for the serialized string.
+Fallbacks to text/plain if not.
+
+=cut
 sub content_type {'text/plain'}
 
-# most serializer don't have to overload this one
+=method support_content_type
+
+Most serializer don't have to overload this one. The method receives a
+content type string and should return a true value if the serializer
+is able to deserialize it.
+
+=cut
 sub support_content_type {
     my ($self, $ct) = @_;
     return unless $ct;
