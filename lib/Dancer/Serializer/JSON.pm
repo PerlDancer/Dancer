357b6bdfbf87f9c1dad7de967d1126be838ebc9c
ambs <ambs@cpan.org>
2011-06-13 18:38:17 +0100

    serializer json

--- lib/Dancer/Serializer/JSON.pm
+++ lib/Dancer/Serializer/JSON.pm
@@ -1,99 +1,6 @@
 package Dancer::Serializer::JSON;
 # ABSTRACT: JSON serializer engine
 
-use strict;
-use warnings;
-use Carp;
-use Dancer::ModuleLoader;
-use Dancer::Deprecation;
-use Dancer::Config 'setting';
-use base 'Dancer::Serializer::Abstract';
-
-
-# helpers
-
-sub from_json {
-    my $s = Dancer::Serializer::JSON->new;
-    $s->deserialize(@_);
-}
-
-sub to_json {
-    my $s = Dancer::Serializer::JSON->new;
-    $s->serialize(@_);
-}
-
-# class definition
-
-sub loaded { Dancer::ModuleLoader->load('JSON') }
-
-sub init {
-    my ($self) = @_;
-    croak 'JSON is needed and is not installed'
-      unless $self->loaded;
-}
-
-sub serialize {
-    my $self   = shift;
-    my $entity = shift;
-
-    my $options = $self->_options_as_hashref(@_) || {};
-
-    # Why doesn't $self->config have this?
-    my $config = setting('engines') || {};
-    $config = $config->{JSON} || {};
-
-    if ( $config->{allow_blessed} && !defined $options->{allow_blessed} ) {
-        $options->{allow_blessed} = $config->{allow_blessed};
-    }
-    if ( $config->{convert_blessed} ) {
-        $options->{convert_blessed} = $config->{convert_blessed};
-    }
-
-    if (setting('environment') eq 'development' and not defined $options->{pretty}) {
-        $options->{pretty} = 1;
-    }
-
-    JSON::to_json( $entity, $options );
-}
-
-sub deserialize {
-    my $self   = shift;
-    my $entity = shift;
-
-    my $options = $self->_options_as_hashref(@_);
-    JSON::from_json( $entity, $options );
-}
-
-sub _options_as_hashref {
-    my $self = shift;
-
-    return if scalar @_ == 0;
-
-    if ( scalar @_ == 1 ) {
-        return shift;
-    }
-    elsif ( scalar @_ % 2 ) {
-        carp "options for to_json/from_json must be key value pairs (as a hashref)";
-    }
-    else {
-        Dancer::Deprecation->deprecated(
-            version => '1.3002',
-            message => 'options as hash for to_json/from_json is DEPRECATED. please pass a hashref',
-        );
-        return { @_ };
-    }
-}
-
-sub content_type {'application/json'}
-
-1;
-__END__
-
-=head1 NAME
-
-Dancer::Serializer::JSON - serializer for handling JSON data
-
-=head1 SYNOPSIS
 
 =head1 DESCRIPTION
 
@@ -118,17 +25,125 @@ extra settings to the B<engines> configuration to turn these on.
             allow_blessed: '1'
             convert_blessed: '1'
 
+=cut
 
-=head1 METHODS
+use strict;
+use warnings;
+use Carp;
+use Dancer::ModuleLoader;
+use Dancer::Deprecation;
+use Dancer::Config 'setting';
+use base 'Dancer::Serializer::Abstract';
 
-=head2 serialize
+sub init {
+    my ($self) = @_;
+    croak 'JSON is needed and is not installed'
+      unless $self->loaded;
+}
+
+
+
+=method from_json
+
+Helper subroutine deserialize a JSON string into a Perl data structure.
+
+=cut
+sub from_json {
+    my $s = Dancer::Serializer::JSON->new;
+    $s->deserialize(@_);
+}
+
+=method to_json
+
+Helper subroutine serializes Perl data structures into a JSON string.
+
+=cut
+sub to_json {
+    my $s = Dancer::Serializer::JSON->new;
+    $s->serialize(@_);
+}
+
+=method loaded
+
+Checks if JSON serializer can be loaded.
+
+=cut
+sub loaded { Dancer::ModuleLoader->load('JSON') }
+
+=method serialize
 
 Serialize a data structure to a JSON structure.
 
-=head2 deserialize
+=cut
+sub serialize {
+    my $self   = shift;
+    my $entity = shift;
+
+    my $options = $self->_options_as_hashref(@_) || {};
+
+    # Why doesn't $self->config have this?
+    my $config = setting('engines') || {};
+    $config = $config->{JSON} || {};
+
+    if ( $config->{allow_blessed} && !defined $options->{allow_blessed} ) {
+        $options->{allow_blessed} = $config->{allow_blessed};
+    }
+    if ( $config->{convert_blessed} ) {
+        $options->{convert_blessed} = $config->{convert_blessed};
+    }
+
+    if (setting('environment') eq 'development' and not defined $options->{pretty}) {
+        $options->{pretty} = 1;
+    }
+
+    JSON::to_json( $entity, $options );
+}
+
+=method deserialize
 
 Deserialize a JSON structure to a data structure
 
-=head2 content_type
+=cut
+sub deserialize {
+    my $self   = shift;
+    my $entity = shift;
 
-Return 'application/json'
+    my $options = $self->_options_as_hashref(@_);
+    JSON::from_json( $entity, $options );
+}
+
+=method content_type
+
+Returns JSON content type string.
+
+=cut
+sub content_type {'application/json'}
+
+
+# privates
+
+sub _options_as_hashref {
+    my $self = shift;
+
+    return if scalar @_ == 0;
+
+    if ( scalar @_ == 1 ) {
+        return shift;
+    }
+    elsif ( scalar @_ % 2 ) {
+        carp "options for to_json/from_json must be key value pairs (as a hashref)";
+    }
+    else {
+        Dancer::Deprecation->deprecated(
+            version => '1.3002',
+            message => 'options as hash for to_json/from_json is DEPRECATED. please pass a hashref',
+        );
+        return { @_ };
+    }
+}
+
+
+1;
+
+
+
