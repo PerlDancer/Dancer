91504450a85c33a33488b8e264fdb3f0874f5a20
ambs <ambs@cpan.org>
2011-06-13 18:47:22 +0100

    Serializer yaml

--- lib/Dancer/Serializer/YAML.pm
+++ lib/Dancer/Serializer/YAML.pm
@@ -7,23 +7,6 @@ use Carp;
 use Dancer::ModuleLoader;
 use base 'Dancer::Serializer::Abstract';
 
-# helpers
-
-sub from_yaml {
-    my ($yaml) = @_;
-    my $s = Dancer::Serializer::YAML->new;
-    $s->deserialize($yaml);
-}
-
-sub to_yaml {
-    my ($data) = @_;
-    my $s = Dancer::Serializer::YAML->new;
-    $s->serialize($data);
-}
-
-# class definition
-
-sub loaded { Dancer::ModuleLoader->load('YAML') }
 
 sub init {
     my ($self) = @_;
@@ -31,39 +14,62 @@ sub init {
       unless $self->loaded;
 }
 
+
+=method from_yaml
+
+Helper subroutine, deserializes a YAML string.
+
+=cut
+sub from_yaml {
+    my ($yaml) = @_;
+    my $s = Dancer::Serializer::YAML->new;
+    $s->deserialize($yaml);
+}
+
+=method to_yaml
+
+Helper subroutine, serializes to a YAML string.
+
+=cut
+sub to_yaml {
+    my ($data) = @_;
+    my $s = Dancer::Serializer::YAML->new;
+    $s->serialize($data);
+}
+
+=method loaded
+
+Checks if the YAML serializer can be loaded.
+
+=cut
+sub loaded { Dancer::ModuleLoader->load('YAML') }
+
+=method serialize
+
+Serializes a data structure to a YAML structure.
+
+=cut
 sub serialize {
     my ($self, $entity) = @_;
     YAML::Dump($entity);
 }
 
+=method deserialize
+
+Deserializes a YAML structure to a data structure
+
+=cut
 sub deserialize {
     my ($self, $content) = @_;
     YAML::Load($content);
 }
 
+=method content_type
+
+Returns 'text/x-yaml'
+
+=cut
 sub content_type {'text/x-yaml'}
 
 1;
 __END__
-
-=head1 NAME
-
-Dancer::Serializer::YAML - serializer for handling YAML data
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-=head1 METHODS
-
-=head2 serialize
-
-Serialize a data structure to a YAML structure.
-
-=head2 deserialize
-
-Deserialize a YAML structure to a data structure
-
-=head2 content_type
-
-Return 'text/x-yaml'
