90dc485432f2e5581fe749c6ea7dca6b4df460ee
ambs <ambs@cpan.org>
2011-07-03 21:35:38 +0100

    fix pod

--- lib/Dancer.pm
+++ lib/Dancer.pm
@@ -959,7 +959,7 @@ This method is deprecated. Use C<set>:
 
 =head2 logger
 
-Deprecated. Use C<set logger => 'console'> to change current logger engine.
+Deprecated. Use C<<set logger => 'console'>> to change current logger engine.
 
 =head2 load
 
