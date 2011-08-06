PerlDancer/Dancer

commit 1edcd37f9de63a9a590fd59bd2dfe6d5e81e1333

Handle spaces in filename for send_file.
tylerdu on IRC reported that filenames with spaces in don't work - thanks
tylerdu!

lib/Dancer.pm
@@ -341,7 +341,9 @@ sub _send_file {
         }
     }
     if (exists($options{filename})) {
-        $resp->push_header('Content-Disposition' => "attachment; filename=$options{filename}");
+        $resp->push_header('Content-Disposition' => 
+            "attachment;filename=\"$options{filename}\""
+        );
     }
     return $resp if $resp;
 

commit cf083da8b1f95110c8ed1be6e9a8c139b016bf2b

Restore space in Content-Disposition header.
My change to quote filenames in the Content-Disposition header in commit
1edcd37f inadvertently removed the space between the attachment & filename parts
of the header, which, whilst still valid, caused a test failure.

lib/Dancer.pm
@@ -342,7 +342,7 @@ sub _send_file {
     }
     if (exists($options{filename})) {
         $resp->push_header('Content-Disposition' => 
-            "attachment;filename=\"$options{filename}\""
+            "attachment; filename=\"$options{filename}\""
         );
     }
     return $resp if $resp;