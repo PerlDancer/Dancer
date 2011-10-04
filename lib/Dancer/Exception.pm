fac8e7f43ea8a8733889c303a2c03d7ea3ea8b41
Damien Krotkine <dams@cpan.org>
2011-10-04 08:24:21 +0200

    added documentation

--- lib/Dancer/Exception.pm
+++ lib/Dancer/Exception.pm
@@ -53,7 +53,12 @@ sub continuation (&;@) {
 
 sub raise ($;@) {
     my $exception_name = shift;
-    my $exception = "Dancer::Exception::$exception_name"->new(@_);
+    my $exception;
+    if ($exception_name =~ s/^\+//) {
+        $exception = $exception_name->new(@_);
+    } else {
+        $exception = "Dancer::Exception::$exception_name"->new(@_);
+    }
     $exception->throw();
 }
 
@@ -94,3 +99,137 @@ register_exception(@$_) foreach (
 );
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Dancer::Exception - class for throwing and catching exceptions and continuations
+
+=head1 SYNOPSIS
+
+    use Dancer::Exception qw(:all);
+
+    register_exception('DataProblem',
+                        message_pattern => "test message : %s"
+                      );
+
+    sub do_stuff {
+      raise DataProblem => "we've lost data!";
+    }
+
+    try {
+      do_stuff()
+    } catch {
+      # an exception was thrown
+      my ($exception) = @_;
+      if ($exception->does('DataProblem')) {
+        # handle the data problem
+        my $message = $exception->message();
+      } else {
+        $exception->rethrow
+      }
+    };
+
+
+
+=head1 DESCRIPTION
+
+Dancer::Exception is based on L<Try::Tiny>. With this module (and
+Dancer::Continuation) you can try and catch exceptions, like in L<Try::Tiny>.
+
+Exceptions are objects, from subclasses of L<Dancer::Exception::Base>.
+
+However, for internal Dancer usage, we introduce a special class of exceptions,
+called L<Dancer::Continuation>. Exceptions that are from this class are not
+caught with a C<catch> block, but only with a C<continuation>. That's a cheap
+way to implement a I<workkflow interruption>. Dancer users should dafely ignore
+this feature
+
+=head2 What it means for Dancer users
+
+Users can throw and catch exceptions, using C<try> and C<catch>. They can reuse
+some Dancer core exceptions (C<Dancer::Exception::Base::*>), but they can also
+create new exception classes, and use them for their own usage. That way it's
+easy to use custom exceptions in a Dancer application. Have a look at
+C<register_exception>, C<raise>, and the methods in L<Dancer::Exception::Base>.
+
+=head2 What it means for Dancer developers
+
+Developers have the tool to implement the fact that calling keywords like C<halt> or
+C<forward> would immediately exit the route in which they are called, without
+the need of prefixing them with C<return>.
+
+=head1 METHODS
+
+=head2 try
+
+Same as in L<Try::Tiny>
+
+=head2 catch
+
+Same as in L<Try::Tiny>. The exception can be retrieved as the first parameter:
+
+    try { ... } catch { my ($exception) = @_; };
+
+=head2 continuation
+
+To be used by Dancer developers only, in Dancer core code.
+
+=head2 raise
+
+  # raise Dancer::Exception::Base::MyException
+  raise MyException => "user $username is unknown";
+
+  # raise My::Own::Exception
+  raise '+My::Own::Exception' => "user $username is unknown";
+
+raise provides an easy way to throw an exception. First parameter is the name
+of the exception class, without the C<Dancer::Exception::Base::> prefix. other
+parameters are stored as I<raising arguments> in the exception. Usually the
+parameters is an exception message, but it's left to the exception class
+implementation.
+
+If the exception calss name starts with a C<+>, then the
+C<Dancer::Exception::Base::> won't be added. This allows to build their own
+exception class herarchy, but you should first look at C<register_exception>.
+If you wish to build your own exception class hierarchy, we recommend that all
+exceptions inherits of L<Dancer::Exception::Base>. Or at least it should
+implement its methods
+
+=head2 register_exception
+
+This method allows to register custom exceptions, usable by Dancer users in
+their route code (actually pretty much everywhere).
+
+  # simple exception
+  register_exception ('InvalidCredentials',
+                      message_pattern => "invalid credentials : %s",
+                     );
+
+This registers a new custom exception. To use it, do:
+
+  raise InvalidCredentials => "user Herbert not found";
+
+The exception message can be retrieved with the C<$exception->message> method, and we'll be
+C<"invalid credentials : user Herbert not found"> (see methods in L<Dancer::Exception::Base>)
+
+  # complex exception
+  register_exception ('InvalidLogin',
+                      composed_from => [qw(Fatal InvalidCredentials)],
+                      message_pattern => "wrong login or password",
+                   );
+
+In this example, the C<InvalidLogin> is built as a composition of the C<Fatal>
+and C<InvalidCredentials> exceptions. See the C<does> method in
+L<Dancer::Exception::Base>.
+
+=head2 registered_exceptions
+
+  my @exception_classes = registered_exceptions;
+
+Returns the list of exception class names. It will list core exceptions C<and>
+custom exceptions (except the one you've registered with a leading C<+>, see
+C<register_exception>). The list is sorted.
--- lib/Dancer/Exception/Base.pm
+++ lib/Dancer/Exception/Base.pm
@@ -71,3 +71,74 @@ sub _raised_arguments {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Dancer::Exception::Base - the base class of all Dancer exceptions
+
+=head1 DESCRIPTION
+
+Dancer::Exception::Base is the base class of all Dancer exception. All core
+exceptions, and all custom exception registered using
+C<Dancer::Exception::register_exception> inherits of
+C<Dancer::Exception::Base>.
+
+=head1 METHODS
+
+=head2 throw
+
+Throws an exception. It's what C<raise> (from L<Dancer::Exception>) uses. Any
+arguments is set as raising parameters. You should not use this method
+directly, but instead, use C<raise> from L<Dancer::Exception>.
+
+B<Warning>, if you want to rethrow an exception, use C<rethrow>.
+
+=head2 rethrow
+
+Re-throw the exception, without touching its parameters. Useful if you've
+caught and exception but don't want to handle it, and want to rethrow it.
+
+  try { ... }
+  catch {
+    my ($e) = @_;
+    $e->does('InvalidLogin')
+      or $e->rethrow;
+    ...
+  };
+
+=head2 does
+
+Given an exception type, returns true if the exception is of the same type.
+
+  try { raise InvalidLogin => 'foo'; }
+  catch {
+    my ($e) = @_;
+    $e->does('InvalidLogin') # true
+    ...
+  };
+
+It can receive more than one type, useful for composed exception, or checking
+multiple types at once. C<does> performs a logical OR between them:
+
+  try { raise InvalidPassword => 'foo'; }
+  catch {
+    my ($e) = @_;
+    $e->does('InvalidLogin', 'InvalidPassword') # true
+    ...
+  };
+
+=head2 get_composition
+
+Returns the type or the composed types of an exception.
+B<Warning>, the result is a list, so you should call this method in list context.
+
+  try { raise InvalidPassword => 'foo'; }
+  catch {
+    my ($e) = @_;
+    my @list = $e->get_composition()
+    # @list contains ( 'InvalidPassword' )
+  };
