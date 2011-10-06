6066a9e77edcf5daf1af803f9da60ec157b6915a
Damien Krotkine <dams@cpan.org>
2011-10-06 22:50:47 +0200

    have forward use continuation

---
+++ lib/Dancer/Continuation/Route.pm
@@ -0,0 +1,9 @@
+package Dancer::Continuation::Route;
+
+use strict;
+use warnings;
+use Carp;
+
+use base qw(Dancer::Continuation);
+
+1;
---
+++ lib/Dancer/Continuation/Route/Passed.pm
@@ -0,0 +1,9 @@
+package Dancer::Continuation::Route::Passed;
+
+use strict;
+use warnings;
+use Carp;
+
+use base qw(Dancer::Continuation::Route);
+
+1;
