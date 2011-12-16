fd3cd6ad41576b8b7a75dff65b21d5437b4e28a4
Sam Kington <github@illuminated.co.uk>
2011-12-16 01:03:31 +0000

    Don't assume references are blessed.

--- lib/Dancer/Exception.pm
+++ lib/Dancer/Exception.pm
@@ -3,6 +3,7 @@ package Dancer::Exception;
 use strict;
 use warnings;
 use Carp;
+use Scalar::Util qw(blessed);
 
 our $Verbose = 0;
 
@@ -26,12 +27,12 @@ sub catch (&;@) {
     my @new_rest = grep { ref ne 'Try::Tiny::Catch' or $continuation_code = $$_, 0 } @rest;
     $continuation_code
       and return ( bless( \ sub {
-          ref && $_->isa('Dancer::Continuation')
+          ref && blessed($_) && $_->isa('Dancer::Continuation')
             ? $continuation_code->(@_) : $block->(@_);
       },  'Try::Tiny::Catch') , @new_rest);
 
     return ( bless ( \ sub {
-          ref && $_->isa('Dancer::Continuation')
+          ref && blessed($_) && $_->isa('Dancer::Continuation')
             ? die($_) : $block->(@_) ;
       }, 'Try::Tiny::Catch'), @new_rest );
 }
@@ -43,7 +44,7 @@ sub continuation (&;@) {
     my @new_rest = grep { ref ne 'Try::Tiny::Catch' or $catch_code = $$_, 0 } @rest;
     $catch_code 
       and return ( bless( \ sub {
-          ref && $_->isa('Dancer::Continuation')
+          ref && blessed($_) && $_->isa('Dancer::Continuation')
             ? $block->(@_) : $catch_code->(@_);
       },  'Try::Tiny::Catch') , @new_rest);
 
