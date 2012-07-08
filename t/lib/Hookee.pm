a6f4ed649d4065a877b012dd01f1b721174c51cb
Alexis Sukrieh <sukria@sukria.net>
2012-07-08 12:03:30 +0200

    new test plugin for home-made hooks

---
+++ t/lib/Hookee.pm
@@ -0,0 +1,14 @@
+package t::lib::Hookee;
+
+use Dancer::Plugin;
+
+register_hook 'start_hookee', 'stop_hookee';
+register_hook 'third_hook';
+
+register some_keyword => sub {
+    execute_hooks('start_hookee');
+};
+
+register_plugin for_versions => [ 2 ];
+
+1;
