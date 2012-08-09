39b0f6b6cabea60f9cffaf35b0f500fc891acd18
Sawyer X <xsawyerx@cpan.org>
2012-08-09 12:43:36 +0300

    use Storable::dclone instead, because it's core

--- lib/Dancer/Template/Abstract.pm
+++ lib/Dancer/Template/Abstract.pm
@@ -3,7 +3,7 @@ package Dancer::Template::Abstract;
 use strict;
 use warnings;
 use Carp;
-use Clone;
+use Storable 'dclone';
 
 use Dancer::Logger;
 use Dancer::Factory::Hook;
@@ -146,18 +146,18 @@ sub _prepare_tokens_options {
     $tokens ||= {};
     $tokens->{perl_version}   = $];
     $tokens->{dancer_version} = $Dancer::VERSION;
-    $tokens->{settings}       = Clone::clone(Dancer::Config->settings);
+    $tokens->{settings}       = dclone(Dancer::Config->settings);
 
     # If we're processing a request, also add the request object, params and
     # vars as tokens:
     if (my $request = Dancer::SharedData->request) {
         $tokens->{request}        = $request;
-        $tokens->{params}         = Clone::clone($request->params);
-        $tokens->{vars}           = Clone::clone(Dancer::SharedData->vars);
+        $tokens->{params}         = dclone($request->params);
+        $tokens->{vars}           = dclone(Dancer::SharedData->vars);
     }
 
     Dancer::App->current->setting('session')
-      and $tokens->{session} = Clone::clone(Dancer::Session->get);
+      and $tokens->{session} = dclone(Dancer::Session->get);
 
     return ($tokens, $options);
 }
