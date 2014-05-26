6022f3f9f5e778719457208af189f7e2f7b4b60e
Doug Schrag <doug@theschrags.net>
2014-05-26 16:01:17 -0400

    add a function to do a deep hash merge

--- lib/Dancer/Config.pm
+++ lib/Dancer/Config.pm
@@ -233,8 +233,7 @@ sub load_settings_from_yaml {
 
     for my $key (keys %{$config}) {
         if ($MERGEABLE{$key}) {
-            my $setting = setting($key);
-            $setting->{$_} = $config->{$key}{$_} for keys %{$config->{$key}};
+            merge_deep(setting($key), $config->{$key});
         }
         else {
             _set_setting($key, $config->{$key});
@@ -242,6 +241,42 @@ sub load_settings_from_yaml {
     }
 
     return scalar(keys %$config);
+}
+
+sub merge_deep {
+  my ($merge, $new, @cycle) = @_;
+
+  # This checks for reference cycles using a standard method:
+  # Traverse the list using two pointers, one advancing one step,
+  # the other advancing two. If there are cycles in the list,
+  # eventually both pointers will point to the same node.
+  my ($history_node, $check);
+  if ($check = shift @cycle) { # check is done every second level
+    # if we are doing the cycle test, advance the history node one key
+    $history_node = $check->{ shift @cycle };
+  } else { # a false flag prevented the check
+    $history_node = shift @cycle || $new;
+  }
+  die 'Cycle detected!'
+    if $check && $new eq $check;
+
+  for my $key (keys %{ $new }) {
+    if (ref $new->{$key} eq 'HASH') {
+      if (exists $merge->{$key}) {
+        die 'Attempted to merge hash with non-hash!'
+          if ref $merge->{$key} ne 'HASH';
+        merge_deep($merge->{$key}, $new->{$key},
+          ($check
+            ? (0)
+            : ()
+          ),
+          $history_node,
+          @cycle, $key); # history of past nodes in chain
+        next;
+      }
+    }
+    $merge->{$key} = $new->{$key};
+  }
 }
 
 sub load_default_settings {
