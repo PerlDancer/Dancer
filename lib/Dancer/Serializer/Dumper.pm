cb34443dce8b78ac0c7f0d8811b16a72f677ab35
Yanick Champoux <yanick@babyl.dyndns.org>
2015-04-20 19:09:53 -0400

    Build results of bde9c45 (on devel)

--- lib/Dancer/Serializer/Dumper.pm
+++ lib/Dancer/Serializer/Dumper.pm
@@ -1,6 +1,9 @@
 package Dancer::Serializer::Dumper;
+BEGIN {
+  $Dancer::Serializer::Dumper::AUTHORITY = 'cpan:SUKRIA';
+}
 # ABSTRACT: Data::Dumper serialisation for Dancer
-
+$Dancer::Serializer::Dumper::VERSION = '1.3135';
 use strict;
 use warnings;
 use Carp;
@@ -38,3 +41,30 @@ sub deserialize {
 sub content_type {'text/x-data-dumper'}
 
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dancer::Serializer::Dumper - Data::Dumper serialisation for Dancer
+
+=head1 VERSION
+
+version 1.3135
+
+=head1 AUTHOR
+
+Dancer Core Developers
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Alexis Sukrieh.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
